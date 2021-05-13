d76705eb2207a7d4ef7fb307eec6455976c8e820
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-05-13 15:23:30 +0300

    test Jenkins

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Smartybox skeleton one
+          Smartybox skeleton two
         </p>
       </header>
     </div>
