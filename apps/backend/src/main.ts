755b8335721438e249a84cf25ffa5c55a9babdd5
Petrica Nanca <petrica_nanca@yahoo.com>
2023-02-15 15:39:07 +0200

    Update main.ts

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,7 +5,7 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+  res.send({ message: 'Hello Smarty API' });
 });
 
 app.listen(port, () => {
