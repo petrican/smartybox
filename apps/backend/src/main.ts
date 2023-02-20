fc2a22e37c82c971858e167cc0f1f0516ee72c2c
Petrica Nanca <petrica_nanca@yahoo.com>
2023-02-20 12:24:35 +0200

    Rem deprecated and update dbseed

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,14 +1,12 @@
 import "reflect-metadata";
-import { createConnection } from "typeorm";
 import express from "express";
 import * as bodyParser from "body-parser";
 
-// import { AdministratorDbSeed } from "./utils/administrator.dbseed";
 import cors from "cors";
-import { allRoutes } from "./routes/index";
-import { AppDataSource } from "./data-source";
+import * as allRoutes from "./routes/index";
+import {AdministratorDbSeed} from "./utils/administrator.dbseed";
 
-AppDataSource.initialize().then(async () => {
+
     // create express app
     const app = express();
     app.use(bodyParser.json());
@@ -20,16 +18,18 @@ AppDataSource.initialize().then(async () => {
 
   // start express server
   if (process.env.SECRET_KEY) {
-    app.listen(5000);
-    // await new AdministratorDbSeed(connection).createAdmin();
-    console.log(`Express server has started on port 5000.`);
+    app.listen(5001);
+    (async () => {
+      await new AdministratorDbSeed().createAdmin();
+    })();
+    console.log(`Express server has started on port 5001.`);
   } else {
     console.log(
       `Please set the SECRET_KEY env first. => export SECRET_KEY=somekey `
     );
     process.exit(1);
   }
-}).catch(error => console.log(error));
+
 
 
 
--- apps/backend/src/utils/administrator.dbseed.ts
+++ apps/backend/src/utils/administrator.dbseed.ts
@@ -1,27 +1,28 @@
-import {Connection, FindOneOptions, getRepository} from "typeorm";
 import { User } from "../entity/User";
 import { Md5 } from "ts-md5/dist/md5";
+import {AppDataSource} from "../data-source";
 
 export class AdministratorDbSeed {
-  constructor(private connection: Connection) {}
 
   async createAdmin() {
-    const userRepository = getRepository(User);
-    let adminUser = await userRepository.findOne({ user_login: "admin" } as FindOneOptions);
+    AppDataSource.initialize().then(async () => {
+      const userRepository = AppDataSource.getRepository(User);
+      const adminUser = await userRepository.findOne({ where: { user_login: "admin" }});
 
-    if (!adminUser) {
-      // insert new users for test
-      await this.connection.manager.save(
-        this.connection.manager.create(User, {
-          user_login: "admin",
-          user_pass: Md5.hashStr("password"),
-          user_fullname: "Administrator",
-          user_role: "administrator",
-          user_email: "admin@admin.com",
-          user_activation_key: "123456",
-          user_status: 1,
-        })
-      );
-    }
+      if (!adminUser) {
+        // insert new users for test
+        await AppDataSource.manager.save(
+          AppDataSource.manager.create(User, {
+            user_login: "admin",
+            user_pass: Md5.hashStr("password"),
+            user_fullname: "Administrator",
+            user_role: "administrator",
+            user_email: "admin@admin.com",
+            user_activation_key: "123456",
+            user_status: 1,
+          })
+        );
+      }
+    }).catch(error => console.log(error));
   }
 }
