5a8e2942c5e5a284a83a3aca3e65d1ae192907a8
Petrica Nanca <petrica_nanca@yahoo.com>
2023-04-25 12:11:03 +0300

    Push admin structure

---
+++ apps/admin/src/App.tsx
@@ -0,0 +1,12 @@
+import type { Component } from 'solid-js';
+import styles from './App.module.css';
+
+const App: Component = () => {
+  return (
+    <div>
+      <header class={styles.header}>Solid App NX </header>
+    </div>
+  );
+};
+
+export default App;
