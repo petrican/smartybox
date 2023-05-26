105e2328a81b69e10c733351c92c645d1c25d586
Petrica Nanca <petrica_nanca@yahoo.com>
2023-05-26 15:15:48 +0300

    Add the router and the solid-devtools entries

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
-
+import devtools from 'solid-devtools/vite'
 export default defineConfig({
   plugins: [
+    devtools({
+      /* additional options */
+      autoname: true, // e.g. enable autoname
+      locator: {
+        targetIDE: 'vscode',
+        componentLocation: true,
+        jsxLocation: true,
+      },
+    }),
     solidPlugin(),
     viteTsConfigPaths({
       root: '../../',
