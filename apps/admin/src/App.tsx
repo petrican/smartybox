b3f4c0c37acf7d50ab532ac97bba2329f6891ade
Petrica Nanca <petrica_nanca@yahoo.com>
2023-06-12 16:16:44 +0300

    Center the login box

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -4,16 +4,20 @@ import Header from "./common-ui/Header/Header";
 import NotFound from "./common-ui/NotFound/NotFound";
 import LoginForm from "./common-ui/LoginForm/LoginForm";
 
+import styles from './App.module.scss';
+
 const App: Component = () => {
   return (
     <div>
       <Header />
-      <Routes>
-        {/*<Route path="/users" component={Users} />*/}
-        {/*<Route path="/" component={Home} />*/}
-        <Route path="/login" component={LoginForm} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <div class={styles['container']}>
+        <Routes>
+          {/*<Route path="/users" component={Users} />*/}
+          {/*<Route path="/" component={Home} />*/}
+          <Route path="/login" component={LoginForm} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
     </div>
   );
 };
