99094525ae28fd4a01200d48846fec17dcb93619
Petrica Nanca <petrica_nanca@yahoo.com>
2023-08-28 07:58:54 +0300

    Upgrade NX 16.7.4

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
-import viteTsConfigPaths from 'vite-tsconfig-paths';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import devtools from 'solid-devtools/vite'
 export default defineConfig({
   plugins: [
@@ -14,9 +14,7 @@ export default defineConfig({
       },
     }),
     solidPlugin(),
-    viteTsConfigPaths({
-      root: '../../',
-    }),
+    nxViteTsPaths(),
   ],
   server: {
     port: 3000,
