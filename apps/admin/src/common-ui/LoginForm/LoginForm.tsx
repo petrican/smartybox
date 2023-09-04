83149fb30dae26b8169f5e5f6ccd9bc956d5a7f6
Petrica Nanca <petrica_nanca@yahoo.com>
2023-09-04 12:20:01 +0300

    Add form validation

--- apps/admin/src/common-ui/LoginForm/LoginForm.tsx
+++ apps/admin/src/common-ui/LoginForm/LoginForm.tsx
@@ -2,12 +2,60 @@ import styles from './LoginForm.module.scss';
 import {createSignal,Component, onMount} from "solid-js";
 import Title from "../Title/Title";
 
+type FormData = {
+  username: string;
+  password: string;
+};
+
+type FormErrors = {
+  username: string;
+  password: string;
+};
+
+
 const LoginForm: Component = () => {
+  const [formData, setFormData] = createSignal<FormData>({
+    username: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = createSignal<FormErrors>({
+    username: "",
+    password: "",
+  });
+
   const [isLoaded, setIsLoaded] = createSignal(false);
 
   onMount(() => {
     setTimeout(() => { setIsLoaded(true); }, 1000);
   })
+
+  const validateForm = (): boolean => {
+    const newErrors: FormErrors = {
+      username: formData().username.trim() === "" ? "Username is required." : "",
+      password:
+        formData().password.length < 6
+          ? "Password must be at least 6 characters."
+          : "",
+    };
+
+    setErrors(newErrors);
+
+    return Object.values(newErrors).every((error) => error === "");
+  };
+
+
+  const handleSubmit = (e: Event) => {
+    e.preventDefault();
+
+    if (validateForm()) {
+      console.log('F:', formData())
+    } else {
+      console.log('Form contains errors')
+    }
+
+  };
+
 
   return (
     <div class={styles[isLoaded() ? 'login-form' : 'login-form-hidden']}>
@@ -17,12 +65,28 @@ const LoginForm: Component = () => {
         fontWeight="600"
         cssClass={styles['login-form-title']}
       />
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          <input type="text" name="username" placeholder="Username" autofocus={true} />
+          <input type="text"
+                 name="username"
+                 placeholder="Username"
+                 autofocus={true}
+                 value={formData().username}
+                 onInput={(e) => setFormData({ ...formData(), username: e.target.value })}
+          />
+          <span class={styles['error-message']}>{errors().username}</span>
         </div>
         <div>
-          <input type="password" name="password" placeholder="Password" />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formData().password}
+            onInput={(e) =>
+              setFormData({ ...formData(), password: e.target.value })
+            }
+          />
+          <span class={styles['error-message']}>{errors().password}</span>
         </div>
         <div>
           <button type="submit">Login</button>
